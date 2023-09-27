0b60e7117c0ea6015c15224335d0f1d1d1b92ca1
rifdqi <rifqi.akbar1999@gmail.com>
2023-09-27 12:55:10 +0700

    Modul 3

---
+++ mod.py
@@ -0,0 +1,13 @@
+s = "Hactive8 Python"
+a = [100,200,300]
+
+def foo(arg):
+    print(f'arg = {arg}')
+    
+class Foo:
+    pass
+
+if (__name__ == '__main__'):
+    print(s)
+    print(a)
+    foo([1,2,3])
